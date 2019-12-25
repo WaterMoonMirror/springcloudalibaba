0353ea08eb5f8590005b6d7982f636ed9bb11685
天空鸟 <lizhu0227@163.com>
2019-12-25 12:25:15 +0800

    服务注册到nacos中

--- src/main/java/com/wondersgroup/lz/usercenter/TestUserController.java
+++ src/main/java/com/wondersgroup/lz/usercenter/TestUserController.java
@@ -2,23 +2,27 @@ package com.wondersgroup.lz.usercenter;
 
 import com.wondersgroup.lz.usercenter.dao.user.UserMapper;
 import com.wondersgroup.lz.usercenter.domain.entity.user.User;
-import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TestUserController {
 
-    @Autowired
-    UserMapper userMapper;
-
+ final    UserMapper userMapper;
+final DiscoveryClient discoveryClient;
     @GetMapping("/test")
     public User test(){
-        var user=new User();
+        val user=new User();
         user.setAvatarUrl("xxx");
         user.setBonus(100);
         user.setCreateTime(new Date());
@@ -26,4 +30,13 @@ public class TestUserController {
         var i = userMapper.insertSelective(user);
         return user;
     }
+
+    @GetMapping("test2")
+    public List<ServiceInstance> test2(){
+        var instances = discoveryClient.getInstances("user-centos");
+        return instances;
+    }
+
+
+
 }
